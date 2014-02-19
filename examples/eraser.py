b5cc31080d96e931cec5baef6b512889c3977dfb
xuxiang <dlutxx@gmail.com>
2014-02-19 23:14:10 +0800

    fix

--- examples/eraser.py
+++ examples/eraser.py
@@ -9,13 +9,13 @@ from weibo import Client
 def erase(uid, access_key):
     client = Client(access_key)
     while True:
-        ret = client.user_timeline(uid=uid, count=100)
+        ret = client.status.user_timeline(uid=uid, count=100)
         if not ret['statuses']:
             break
         for st in ret['statuses']:
             print("Deleting %s: %s" % (st['id'], st['text']))
-            client.destroy(st['id'])
+            client.status.destroy(st['id'])
 
 
 if __name__ == '__main__':
-    erase(1625287522, '2.00sWXzlBFsmYkBb8124b55e5p_MHjB')
+    erase(1625287522, '2.00sWXzlBk5pPEC34765094ba9uZ9CD')
