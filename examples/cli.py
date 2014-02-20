66ba0a20efeed9686310a9b7576e2cb380747b80
xuxiang <dlutxx@gmail.com>
2014-02-21 00:03:30 +0800

    save

--- examples/cli.py
+++ examples/cli.py
@@ -2,13 +2,39 @@
 from __future__ import print_function
 
 import weibo
+import curses
+import logging
 
+
+class Command(object):
+    pass
 
 class App(object):
     ''' Terminal weibo client'''
 
-    def __init__(self, ak=None):
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.weibo = weibo.Client(cfg.access_key)
+
+    def dispatch(self, ch):
+        cmd = self.map_key(ch)
+        if cmd:
+            try:
+                self.run_cmd(cmd, ch)
+            except Exception as e:
+                pass
+
+    def map_key(self, ch):
         pass
+
+    def run_cmd(self, cmd, ch):
+        pass
+
+    def mainloop(self, stdscr):
+        self.scr = stdscr
+        while True:
+            ch = stdscr.getch()
+            self.dispatch(ch)
 
 
 class Config(object):
@@ -43,7 +69,8 @@ class Config(object):
 
 
 def main():
-    App().mainloop()
+    app = App(Config.loadfile('~/.weiborc'))
+    curses.wrapper(app.mainloop)
 
 
 if __name__ == '__main__':
